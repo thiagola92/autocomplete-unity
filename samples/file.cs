8601fea2ba476101f524ccf4bf4cbc31d296875c
thiagola92 <thiagola92@gmail.com>
2019-01-26 12:47:02 -0200

    Update

---
+++ samples/file.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+  public void Update() {
+    if(Input.GetButtonDown("Fire1")) {
+      Debug.Log(Input.mousePosition);
+    }
+  }
+}

67fd186e48a57ba6876fa4f9d12c9c84790e960d
thiagola92 <thiagola92@gmail.com>
2019-02-14 05:32:14 -0200

    Fix the replacemente problem after dots Add more API

--- samples/file.cs
+++ samples/file.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class ExampleClass : MonoBehaviour {
+
+  GameObject.CreatePrimitive(type);
+
   public void Update() {
     if(Input.GetButtonDown("Fire1")) {
       Debug.Log(Input.mousePosition);
